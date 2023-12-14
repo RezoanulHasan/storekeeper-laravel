3f88b0110bb2cbeb274fca8d8a6dfb9172c12fc0
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-12-14 21:55:51 +0600

    add product and sale controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('dashboard', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+        ]);
+
+        Product::create($validatedData);
+
+        return redirect()->route('dashboard')->with('success', 'Product added successfully');
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $validatedData = $request->validate([
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update(['price' => $validatedData['price']]);
+
+        return redirect()->route('dashboard')->with('success', 'Product price updated successfully');
+    }
 }
--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    //
+    public function index()
+    {
+        $sales = Sale::all();
+        return view('sales.index', compact('sales'));
+    }
 }
